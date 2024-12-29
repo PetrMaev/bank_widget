7231e0971bebb67a3a6c4a1342d5c937667bfb8a
Petr Maev <petr.maev94@gmail.com>
2024-12-29 21:38:09 +0300

    add tests for decorator 'log'

--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,7 @@
 import os
 from functools import wraps
 from typing import Optional, Callable, Any
+from config import path
 
 
 def log(filename: Optional[str]=None)-> Callable:
@@ -13,10 +14,10 @@ def log(filename: Optional[str]=None)-> Callable:
                 try:
                     result = func(*args, **kwargs)
                 except Exception as error:
-                    with open(os.path.join(f'{filename}.txt'), 'a', encoding='utf-8') as file:
+                    with open(os.path.join(path, f'{filename}.txt'), 'a', encoding='utf-8') as file:
                         file.write(f'{func.__name__} error: {error.__class__.__name__}. Inputs: {args}, {kwargs}\n')
                 else:
-                    with open(os.path.join(f'{filename}.txt'), 'a', encoding='utf-8') as file:
+                    with open(os.path.join(path, f'{filename}.txt'), 'a', encoding='utf-8') as file:
                         file.write(f'{func.__name__} ok\n')
                     return result
             else:
@@ -29,3 +30,20 @@ def log(filename: Optional[str]=None)-> Callable:
                     return result
         return wrapper
     return my_decorator
+
+
+# @log()
+# def example_function(a, b):
+#     """ Складывает два числа"""
+#     return a + b
+
+
+# @log(filename='my_log')
+# def example_function(a, b):
+#     """ Складывает два числа"""
+#     return a + b
+
+# example_function('3', 2)
+# example_function(3, 2)
+# print(example_function(3, 2))
+# example_function()
